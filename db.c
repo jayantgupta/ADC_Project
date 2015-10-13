2880229e8af6a42f3b1c81b271c71b0fe9e8eeaa
Jayant Gupta <jayant27290@gmail.com>
2015-10-12 19:51:43 -0700

    Code for creating the Data base file.

---
+++ db.c
@@ -0,0 +1,103 @@
+/*
+ Author : Jayant Gupta
+ Date : October 12, 2015
+ This is the sub-code containing the 
+ functions related to the dictionary.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdbool.h>
+
+/* Data Structures*/
+char dict[100000][25]; // Data Structure storing the dictionary.
+bool flag[100000];
+
+/* Functions*/
+void init_dict(const char *file_name);
+bool PUT(int key, char * value);
+char * GET(int key);
+bool DELETE(int key);
+
+main(){
+	const char *name="word_list";
+	init_dict(name);
+	printf("%s", GET(8));
+}
+
+// Note: each string has a newline character at the end.
+void init_dict(const char *file_name){
+	FILE *fp;
+	fp=fopen(file_name, "r");
+	char c;
+	if(fp == NULL){
+		printf("Fatal Error:File does not exist\n");
+		exit(EXIT_FAILURE);
+	}
+	const size_t line_size = 300;
+	char *line = malloc(line_size);
+	int index = 0;
+	while(fgets(line, line_size, fp)!= NULL){
+		strcpy(dict[index++], line);
+	}
+	free(line);
+	int i;
+	for(i = 0 ; i < index ; i++){
+		flag[i] = true;
+	}
+	for(i = index ; i < 100000 ; i++){
+		flag[i] = false;
+	}
+	return;	
+}
+
+bool PUT(int key, char *value){
+	if(key >= 0 && key < 100000 ){
+		if(flag[key]){
+			printf("Key exists\n");
+			return false;
+		}
+		else{
+			strcpy(dict[key],value);
+			return true;
+		}
+	}
+	else{
+		printf("Invalid Key\n");
+		return false;
+	}	
+}
+
+char *GET(int key){
+	if(key >= 0 && key < 100000 ){
+		if(flag[key]){
+			return dict[key];
+		}
+		else{
+			printf("Value does not exist\n");
+			return NULL;
+		}
+	}
+	else{
+		printf("Invalid Key\n");
+		return false;
+	}	
+}
+
+bool DELETE(int key){
+	if(key >= 0 && key < 100000 ){
+		if(flag[key]){
+			flag[key] = false;
+			return true;
+		}
+		else{
+			printf("Value does not exist\n");
+			return false;
+		}
+	}
+	else{
+		printf("Invalid Key\n");
+		return false;
+	}	
+}
