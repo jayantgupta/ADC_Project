5c6180e9cecc5b07fd174ad8131b756778f25ca6
Jayant Gupta <jayant27290@gmail.com>
2015-12-07 21:23:11 -0800

    Cleaned and structured code.

--- project_4/acceptor.c
+++ project_4/acceptor.c
@@ -1,7 +1,6 @@
-/* tcpserver.c 
-	 Serving 2-phase commit requests amongst various servers
-	 Initial Version : Alaka Sukumar
-	 2-phase commit version : Jayant Gupta
+/* acceptor.c 
+	 Paxos Acceptor
+	 Authors: Jayant Gupta, Alaka Sukumar
 	 Updated Date : November 13, 2015
  */
 
@@ -14,15 +13,107 @@
 #include <errno.h>
 
 const char *GET_FILE = "GET_buffer";
-char *exec_rqst(char *);
 long unsigned int promised_id;// greatest promised id
 long unsigned int current_accepted_id;// greatest accepted id seen by acceptor
-char *request; // the request to be processed
 const char *INIT_Promise = "INIT_Promise"; // store greatest promised id in this file
 const char *INIT_Accepted = "INIT_Accepted"; // store greatest accepted id in this file
 
+void init(long unsigned int prom_id, long unsigned int accep_id);
+int create_tcp_server(char *port);
+char **get_tokens(char *recv_buff);
+void store_buffer(const char *buffer_file, long unsigned int ID);
+char *learner(char *);
+char *promise_message(long unsigned int promised_id);
+char *accepted_message(long unsigned int current_accepted_id);
+
+int main(int argc, char *argv[]){
+				if(argc != 2){
+								printf("Error\nUsage ./2pc_server.exec port\n");
+								exit(1);
+				}	
+				/*
+					 Setting up the key-value directory.
+				 */
+				int sockfd, newsock, recv_bytes ;
+				struct sockaddr_in client_addr;
+				int sin_size =  sizeof(struct sockaddr_in);
+				const char *log_filename = "tcp_server.log";
+
+				/* call init function to initialize the values */
+				init(promised_id, current_accepted_id);
+				//sync(current_accepted_id); // To start syncing.
+				sockfd = create_tcp_server(argv[1]);
+
+				/* server listening */
+				printf("Server Listening ..\n");
+				fflush(stdout);
+				while(1){
+								char send_buff [1024] , recv_buff[1024];
+								/* accept the connection */
+								newsock = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
+								printf("Accepted Request\n");
+								// Serve first request.Check if sequence number less than its own stored seq number.
+								/* Phase 1 - receive proposal */ 
+
+								recv_bytes = recv(newsock, recv_buff, 1024, 0);
+								recv_buff[recv_bytes] = '\0';
+								printf("%s\n", recv_buff);
+								char ** allTokens = get_tokens(recv_buff);             
+								printf("The receieve proposer id is : %s \n", allTokens[1]);
+							
+								long unsigned int received_prop_id = atol(allTokens[1]);
+
+								/* check if current received proposal id is greater than promised_id*/
+								if (received_prop_id >= promised_id){                        
+												promised_id = received_prop_id; 
+												/* store promised id in INIT_Promise file */
+												store_buffer(INIT_Promise, promised_id);
+								}else{
+									//abort and re-initiate.
+								}
+												
+								/* send promise message */
+								char *promisemessage = promise_message(promised_id);
+								printf("Promise Message is : %s\n", promisemessage);
+								send(newsock, promisemessage, strlen(promisemessage), 0);// Send back the promise.
+
+								/*Phase 2 - Receive Accept  */
+								recv_bytes = recv(newsock, recv_buff, 1024, 0);
+								recv_buff[recv_bytes] = '\0'; 
+								/* process Accept msg */
+								char **allAcceptTokens = get_tokens(recv_buff);             
+								char *accept_id = allAcceptTokens[1];
+								char *request = allAcceptTokens[2];
+								long unsigned int received_accept_id = atol(allAcceptTokens[1]);
+
+								/* check if promised id is still greater than or equal to accept id received from proposer */
+								if (received_accept_id >= promised_id){
+												current_accepted_id = received_accept_id; 
+												/* store accepted id in INIT_Accepted file */
+												store_buffer(INIT_Accepted, current_accepted_id);
+												
+												/* send accepted message to proposer */
+												char *acceptedmessage = accepted_message(current_accepted_id);
+												printf("Accepted Message is : %s\n", acceptedmessage); 
+												send(newsock, acceptedmessage, strlen(acceptedmessage), 0);
+								}
+								else{ 
+									// abort and re-initiate 								
+								}
+
+								/* invoke learner */
+								recv_bytes = recv(newsock, recv_buff, 1024, 0);
+								recv_buff[recv_bytes] = '\0'; 
+								learner(recv_buff); // learner.
+								printf("Request : %s\n", request);
+
+								close(newsock);
+				}
+				close(sockfd);
+				return 0;
+}
 //init method to initialize values on node reboot */
-void init(long unsigned int prom_id,long unsigned int accep_id){
+void init(long unsigned int prom_id, long unsigned int accep_id){
 				/* read and initialize promise id*/
 
 				FILE *fp = fopen(INIT_Promise, "r");
@@ -42,158 +133,83 @@ void init(long unsigned int prom_id,long unsigned int accep_id){
 				printf("Initialized\n"); 
 }
 
-int main(int argc, char *argv[]){
-	if(argc != 2){
-		printf("Error\nUsage ./2pc_server.exec port\n");
-		exit(1);
-	}	
-	/*
-		Setting up the key-value directory.
-	*/
-	int sockfd, newsock, recv_bytes ;
-	bool val = true;
-	struct sockaddr_in server_addr, client_addr;
-	int sin_size;
-        const char *log_filename = "tcp_server.log";
-
-  /* call init function to initialize the values */
-  init(promised_id, current_accepted_id);
-	sync(current_accepted_id); // 
-	
-	/* creating a socket */
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		perror("Error opening socket");
-		exit(1);
-	}
-	if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&val,sizeof(int)) == -1)
-	{
-		perror("Error in setting socket opt");
-		exit(1);
-	}
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(atoi(argv[1]));
-	server_addr.sin_addr.s_addr = INADDR_ANY;
-	bzero(&(server_addr.sin_zero),8);
-	/* bind the socket to an address */
-	if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
-	{
-		perror("Error in binding");
-		exit(1);
-	}
-	/* listen for connections*/
-	if (listen(sockfd, 5) == -1)
-	{
-		perror("Listen");
-		exit(1);
-	}
-
-	/* logging server listening */
-  printf("Server Listening ..\n");
-	fflush(stdout);
-	while(1){
-		char send_buff [1024] , recv_buff[1024];
-		sin_size = sizeof(struct sockaddr_in);
-                 int send_count = 1;
-                 int status ;
-		/* accept the connection */
-		newsock = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
-		printf("Accepted Request\n");
-		// Serve first request.Check if sequence number less than its own stored seq number.
-    /* Phase 1 - receive proposal */ 
-                
-		recv_bytes = recv(newsock, recv_buff, 1024, 0);
-		recv_buff[recv_bytes] = '\0';
-		printf("%s\n", recv_buff);
-                /* split prepare msg on ":" */
-                const char s[2] = ":";
-		char *token;
-		/* get the first token */
-		token = strtok(recv_buff, s); 
-                char allTokens[3][26];             
-		int i = 0;
-		while( token != NULL && i < 3)   
-		{ 
-					 printf("%s\n", token);	
-						strcpy(allTokens[i],token);
-						token = strtok(NULL, s);
-						i++;
-		}
-                 printf("The receieve proposer id is : %s \n", allTokens[1]);
-                 long unsigned int received_prop_id = atol(allTokens[1]);
-                 
-                 /* check if current promised id is equal to or greater than received proposal id */
-                 if (promised_id == received_prop_id || received_prop_id > promised_id){                        
-                 promised_id= received_prop_id; 
-                 /* store promised id in INIT_Promise file */
-                 FILE *fp1 = fopen(INIT_Promise, "w");
-						fprintf(fp1, "%lu", promised_id);
-						fclose(fp1);
-
-
-                /* send promise message */
-                
-                char str[15]; 
-                sprintf(str, "%lu", promised_id);              
-                char promisemessage[50];
-                strcpy(promisemessage,  "promise:");
-                strcat(promisemessage, str);// This is the promise message
-                printf("Promise Message is : %s\n", promisemessage);
-		send(newsock, promisemessage, strlen(promisemessage), 0);// Send back the promise.
-                }
-
-		/*Phase 2 - Receive Accept  */
-		recv_bytes = recv(newsock, recv_buff, 1024, 0);
-		recv_buff[recv_bytes] = '\0'; 
-                /* process Accept msg */
-                const char c[2] = ":";
-		char *acceptstring;
-		/* get the first token */
-		acceptstring = strtok(recv_buff, c); 
-                char allAcceptTokens[3][26];             
-		int j = 0;
-		while( acceptstring != NULL && j < 3)   
-		{ 
-					 printf("%s\n", acceptstring);	
-						strcpy(allAcceptTokens[j],acceptstring);
-						acceptstring = strtok(NULL, c);
-						j++;
-		}
-                char *accept_id = allAcceptTokens[1];
-                request = allAcceptTokens[2];
-                long unsigned int received_accept_id = atol(allAcceptTokens[1]);
-                
-                /* check if promised id is greater than accept id received from proposer */
-                if (promised_id == received_accept_id || received_accept_id > promised_id){
-                current_accepted_id = received_accept_id; 
-                /* store accepted id in INIT_Accepted file */
-                 FILE *fp2 = fopen(INIT_Accepted, "w");
-						fprintf(fp2, "%lu", current_accepted_id);
-						fclose(fp2);
-                /* send accepted message to proposer */
-                char accepted_id_for_msg[15];
-                sprintf(accepted_id_for_msg, "%lu", current_accepted_id); 
-                char acceptedmessage[50];
-                strcpy(acceptedmessage,  "accepted:");
-                
-                strcat(acceptedmessage, accepted_id_for_msg);// This is the accepted message
-                printf("Accepted Message is : %s\n", acceptedmessage); 
-		send(newsock, acceptedmessage, strlen(acceptedmessage), 0);
-                }
-                
-                 /* invoke learner */
-                recv_bytes = recv(newsock, recv_buff, 1024, 0);
-		recv_buff[recv_bytes] = '\0'; 
-		
-			learner(recv_buff); // learner.
-			printf("Request : %s\n", request);
-		
-		close(newsock);
-	}
-	close(sockfd);
-	return 0;
+int create_tcp_server(char *port){
+				int sockfd;
+				bool val = true;
+				struct sockaddr_in server_addr ;/* creating a socket */
+				if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+				{
+								perror("Error opening socket");
+								exit(1);
+				}
+				if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&val,sizeof(int)) == -1)
+				{
+								perror("Error in setting socket opt");
+								exit(1);
+				}
+				server_addr.sin_family = AF_INET;
+				server_addr.sin_port = htons(atoi(port));
+				server_addr.sin_addr.s_addr = INADDR_ANY;
+				bzero(&(server_addr.sin_zero),8);
+				/* bind the socket to an address */
+				if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
+				{
+								perror("Error in binding");
+								exit(1);
+				}
+				/* listen for connections*/
+				if (listen(sockfd, 5) == -1)
+				{
+								perror("Listen");
+								exit(1);
+				}
+				return sockfd;
 }
 
+char **get_tokens(char *recv_buff){
+				/* split prepare msg on ":" */
+				const char s[2] = ":";
+				char *token;
+				/* get the first token */
+				token = strtok(recv_buff, s); 
+				char **allTokens = (char **)malloc(sizeof(char *)*3);             
+				int i = 0;
+				while( token != NULL && i < 3)   
+				{ 
+								allTokens[i] = (char *)malloc(sizeof(char)*26);
+///								printf("%s\n", token);	
+								strcpy(allTokens[i], token);
+								token = strtok(NULL, s);
+								i++;
+				}
+				return allTokens;
+
+}
+
+void store_buffer(const char *buffer_file, long unsigned int ID){
+				FILE *fp = fopen(buffer_file, "w");
+				fprintf(fp, "%lu", ID);
+				fclose(fp);
+}
+
+char *promise_message(long unsigned int promised_id){
+				char str[15]; 
+				sprintf(str, "%lu", promised_id);              
+				char *promisemessage = (char *)malloc(sizeof(char)*50);
+				strcpy(promisemessage,  "promise:");
+				strcat(promisemessage, str);// This is the promise message
+				return promisemessage;
+
+}
+
+char *accepted_message(long unsigned int current_accepted_id){
+				char accepted_id_for_msg[15];
+				sprintf(accepted_id_for_msg, "%lu", current_accepted_id); 
+				char *acceptedmessage = (char *)malloc(sizeof(char)*50);
+				strcpy(acceptedmessage,  "accepted:");
+				strcat(acceptedmessage, accepted_id_for_msg);// This is the accepted message
+				return acceptedmessage;
+}
 
 
 // Takes the first request and responds as ACK.
@@ -215,26 +231,20 @@ char * learner(char * request){
 						i++;
 		}
 		if(strcmp(allTokens[0],"GET") == 0){ 
-//					callLog("Called method GET \n", log_filename);
 						printf("Called method GET\n");
 						int key = atoi(allTokens[1]);
 						char *getValue = _GET(key);
-//					send(newsock, getValue, strlen(getValue), 0);    
 						FILE *fp = fopen(GET_FILE, "w");
 						fputs(getValue, fp);
 						fclose(fp);
 		} 
 		else if(strcmp(allTokens[0],"PUT") == 0){
-//					callLog("Called method PUT \n", log_filename);
 						printf("Inside PUT %s %s\n", allTokens[1], allTokens[2]);
 						bool putValue = _PUT(atoi(allTokens[1]), allTokens[2]); 
 						if(putValue){
-//							callLog("Successfully Updated\n", log_filename);
 								return "COMMIT";
 						}
-
 						else{
-//						callLog("Error in updating \n", log_filename);
 							return "PUT_ERROR";
 						}
 
--- project_4/proposer.c
+++ project_4/proposer.c
@@ -1,26 +1,30 @@
 /*
-	tcpclient.c 
-	TOADD : Timeout.
-	Initial version by Alaka Sukumar
-	Edited by Jayant Gupta to serve
-	the purpose of 2-phase commit. 
-	This will act as a coordinator, and will end the request 
-	for Commits to all the servers, including itself.
-	Date. Nov 13, 2015.
+	proposer.c 
+	PAXOS proposer
+	Authors: Jayant Gupta, Alaka Sukumar
+	Date. Dec 7, 2015.
 */
 #include "proposer.h"
 
 int promise_count;
 int accepted_count;
 pthread_mutex_t thread_lock;
-int count = 2; // Number of nodes.
-int quorum;
 const char *SYSTEM_FILE = "SYSTEM_buffer";
-char c[1000];
-int alivenodes;
+const char *INIT_PROMISE = "INIT_Promise";
   
 /* current highest seq number of proposer */
-long unsigned int current_id = 0;
+long unsigned int current_id = 0; // will it always start from this?
+
+//init method to initialize values on node reboot */
+void init(long unsigned int prom_id){
+				/* read and initialize promise id*/
+
+				FILE *fp = fopen(INIT_PROMISE, "r");
+				fscanf(fp, "%lu", &prom_id); // Copied alive nodes from file.
+				fclose(fp);
+
+				printf("Promise id initialized is  : %lu \n", prom_id);
+}
 
 void get_and_add_promise(){
 			pthread_mutex_lock(&thread_lock);
@@ -34,33 +38,16 @@ void get_and_add_accepted(){
 			pthread_mutex_unlock(&thread_lock);
 }
 
-void *propose(void *addr){
-				struct host_addr{
-								char *IP;
-								char *port;
-								char rqst[1024];
-								bool read_flag;
-				}* t_addr;
-				t_addr = (struct host_addr *)addr;
-				char *IP = t_addr->IP;
-				char *port = t_addr->port;
-				char *request = t_addr->rqst;
-				bool read_flag = t_addr->read_flag;
-				printf("Connect to %s %s with request %s\n", IP, port, request);
-
-				int sockfd, recv_bytes;
-				char send_buff[1024], recv_buff[1024];
+int create_tcp_connection(char *IP, char *port){
+				int sockfd;
+				printf("Connect to %s %s \n", IP, port);
 				struct sockaddr_in server_addr;
 				struct hostent *host = gethostbyname(IP);
-
-
-				fd_set myset;
-				struct timeval tv;
 				if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 								perror("Socket");
 								printf("Unable to create socket\n");
-								//        callLog("error in creating a socket\n", log_filename);
-								pthread_exit(1);
+								int exit_status  = 1;
+								pthread_exit(&exit_status);
 				}
 				/* creating a socket */
 				server_addr.sin_family = AF_INET;
@@ -73,133 +60,147 @@ void *propose(void *addr){
 				{
 								perror("Failed to connect");
 								printf("connection failed\n");
-								pthread_exit(1);
+								int exit_status = 1;
+								pthread_exit(&exit_status);
 				}
-
-				/* Create Time out values */
 				printf("Connection Done !!!");
-				tv.tv_sec = 0 ;
-				tv.tv_usec = 10000;
-				FD_ZERO(&myset);
-				FD_SET(sockfd, &myset);
+				return sockfd;
+}
 
-				/* Get message to send to the server */
-				int send_count = 1;
-				int status ;
-				/* send prepare msg to all the servers */
-				long unsigned int proposal_id = current_id;
-				char str[15];
-				sprintf(str, "%lu", current_id); 
-				printf("The current id string is : %s\n", str);
-				char preparemessage[50];
-				strcpy(preparemessage,  "prepare:");
-				strcat(preparemessage, str);// This is the prepare message
-				printf("prepare msg is %s \n",preparemessage);
-				send(sockfd, preparemessage, strlen(preparemessage), 0);
-				/* receive promise from quorum */
-				recv_bytes = recv(sockfd, recv_buff, 1024, 0);
-				printf("%s--%s-- \n", port, recv_buff);
+int get_quorum(){
+				/* compute quorum by checking number of nodes alive */
+				FILE *fs = fopen(SYSTEM_FILE, "r");
+				char c[10];
+				fscanf(fs, "%s", c); // Copied alive nodes from file.
+				fclose(fs);
+				int alivenodes = atoi(c);
+				return (alivenodes / 2) + 1;
+}
+
+char ** get_tokens(char *recv_buff){
 				/* split promise msg on ":" */
 				const char s[2] = ":";
 				char *token;
 				/* get the first token */
 				token = strtok(recv_buff, s); 
-				char allTokens[3][26];             
+				char **allTokens;
+				allTokens = (char **)malloc(sizeof(char *)*3);   
 				int i = 0;
 				while( token != NULL && i < 3)   
 				{ 
+								allTokens[i] = (char *)malloc(sizeof(char)*26);
 								printf("%s\n", token);	
-								strcpy(allTokens[i],token);
+								strcpy(allTokens[i], token);
 								token = strtok(NULL, s);
 								i++;
 				}
-				char *receivedmsg = allTokens[0];
-				char *promise_id = allTokens[1];
-				/* update proposer's current id to received promise id */
-				long unsigned int received_prom_id = atol(allTokens[1]);
-				current_id = received_prom_id;
+				return allTokens;
+}
 
-				if(strcmp(receivedmsg,"promise" ) == 0){
-								get_and_add_promise();
+char *prepare_message(long unsigned int current_id){
+				char str[15];
+				sprintf(str, "%lu", current_id); 
+				printf("The current id string is : %s\n", str);
+				char *preparemessage = (char *)malloc(sizeof(char) * 50);
+				strcpy(preparemessage,  "prepare:");
+				strcat(preparemessage, str);// This is the prepare message
+				return preparemessage;
+}
 
-				}
-				printf("promise count : %d\n", promise_count);
-				/* compute quorum by checking number of nodes alive */
 
-				FILE *fs = fopen(SYSTEM_FILE, "r");
-				fscanf(fs, "%s", c); // Copied alive nodes from file.
-
-				fclose(fs);
-				alivenodes = atoi(c);
-				quorum = (alivenodes / 2) + 1;
-				while(promise_count < quorum && !read_flag){
-								//spin;
-				}
-
-				/* Send Accept */
-				char acceptmessage[50];
+char *accept_message(char *promise_id, char *request){
+				char *acceptmessage = (char *)malloc(sizeof(char)*50);
 				strcpy(acceptmessage,  "accept:");
 				strcat(acceptmessage, promise_id);// accept with promise id received from acceptors.
 				strcat(acceptmessage, ":");
 				strcat(acceptmessage, request); // this is accept message
+				return acceptmessage;
+}
 
-				printf("accept msg is %s \n",acceptmessage);
+// Executes the propose protocol of Paxos
+void *propose(void *addr){
+				struct host_addr{
+								char *IP;
+								char *port;
+								char rqst[1024];
+								bool read_flag;
+				}* t_addr;
+				t_addr = (struct host_addr *)addr;
+				char *IP = t_addr->IP;
+				char *port = t_addr->port;
+				char *request = t_addr->rqst;
+				bool read_flag = t_addr->read_flag;
+
+				int sockfd, recv_bytes;
+				char send_buff[1024], recv_buff[1024];
+
+				/*Initialize tcp connection */	
+				sockfd = create_tcp_connection(IP, port);
+				int quorum = get_quorum();
+				
+				/* send Prepare to the connected server */
+				long unsigned int proposal_id = current_id;
+				char *preparemessage = prepare_message(current_id);
+
+				printf("prepare msg is %s \n", preparemessage);
+				send(sockfd, preparemessage, strlen(preparemessage), 0); // prepare
+				/* receive promise from quorum */
+				recv_bytes = recv(sockfd, recv_buff, 1024, 0); // prepare response
+				printf("%s:%s:%s \n", IP, port, recv_buff);
+				char ** allTokens = get_tokens(recv_buff);
+				char *receivedmsg = allTokens[0];
+				char *promise_id = allTokens[1];
+				
+				/* update proposer's current id to received promise id */
+				long unsigned int received_prom_id = atol(promise_id);
+				current_id = received_prom_id; // change the current_id to received prom id. ??
+
+				if(strcmp(receivedmsg, "promise" ) == 0){
+								get_and_add_promise();
+				}
+				printf("promise count : %d\n", promise_count);
+				
+				while(promise_count < quorum && !read_flag){
+								//spin;TODO add timeout here.
+				}
+
+				/* Send Accept */
+				char *acceptmessage = accept_message(promise_id, request);
+				printf("accept msg is %s \n", acceptmessage);
 				send(sockfd, acceptmessage, strlen(acceptmessage), 0);
 
 				/* receive accepted by quorum */
 				recv_bytes = recv(sockfd, recv_buff, 1024, 0);
-				printf("%s--%s-- \n", port, recv_buff);
-				/* split accepted msg on ":" */
-				const char a[2] = ":";
-				char *acceptedstrings;
-				/* get the first token */
-				acceptedstrings = strtok(recv_buff, a); 
-				char allAcceptedTokens[3][26];             
-				int j = 0;
-				while( acceptedstrings != NULL && j < 3)   
-				{ 
-								printf("%s\n", acceptedstrings);	
-								strcpy(allAcceptedTokens[j],acceptedstrings);
-								acceptedstrings = strtok(NULL, a);
-								j++;
-				}
+				printf("%s:%s:%s \n", IP, port, recv_buff);
+				char **allAcceptedTokens = get_tokens(recv_buff);            
 				char *receivedacceptedmsg = allAcceptedTokens[0];
 				char *accepted_id = allAcceptedTokens[1];
 				/* update proposer's current id to received accepted id */
 				long unsigned int received_acc_id = atol(allAcceptedTokens[1]);
-				current_id = received_acc_id;
+				current_id = received_acc_id; // Changing current_id to received accepted_id. ??
 
-				if(strcmp(receivedacceptedmsg,"accepted" ) == 0){
+				if(strcmp(receivedacceptedmsg, "accepted" ) == 0){
 								get_and_add_accepted();
-
 				}
 				printf("accepted count : %d\n", accepted_count);
+				
 				/* check if accepted is received by quorum */
-				/* compute quorum by checking number of nodes alive */
-				FILE *fp = fopen(SYSTEM_FILE, "r");
-				fscanf(fp, "%s", c); // Copied alive nodes from file.
-				fclose(fp);
-				alivenodes = atoi(c);
-				printf("THE NUMBER  OF LIVE NODES ARE : %d\n",alivenodes);
-				quorum = (alivenodes/2) + 1;
 				while(accepted_count < quorum && !read_flag){
 								//spin; 
 				}
-				printf("%s : %d\n", port, accepted_count);
+				printf("%s:%s:%d\n", IP, port, accepted_count);
 
-				/* Announce */
+				/* Announce for learner*/
 				send(sockfd, request, strlen(request), 0);
-
 				close(sockfd);
 				pthread_exit(0);
 				return NULL ;
 }
 
-// Coordinator.
+// Proposer.
 // This will be called by rpc_kev_server
 bool run(char request[1024], bool read_flag){
 				printf("%s\n", request);
-				int *status[count];
 				pthread_attr_t attr;
 				pthread_attr_init(&attr);
 				pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
@@ -207,8 +208,7 @@ bool run(char request[1024], bool read_flag){
 				int i, ret; // Count of Peers.
 				char *ip[] = {"localhost", "localhost"}; // add ip here.
 				char *ports[] = {"10000", "10001"};  // add ports here.
-				//		char *ip[] = {"localhost", "localhost"}; // add ip here.
-				//		char *ports[] = {"10000", "10001"};  // add ports here.
+				int count = sizeof(ip) / sizeof(ip[0]);
 				struct host_addr
 				{
 								char *IP;
@@ -219,7 +219,6 @@ bool run(char request[1024], bool read_flag){
 				struct host_addr **addr = (struct host_addr **)malloc(sizeof(struct host_addr*)*count);
 				pthread_t thread_array[count];
 				promise_count = 0;
-				//quorum = (count/2) + 1 ; 
 				accepted_count = 0;
 				for(i = 0 ; i < count ; i++){
 								addr[i] = (struct host_addr *)malloc(sizeof(struct host_addr));
@@ -234,7 +233,7 @@ bool run(char request[1024], bool read_flag){
 								if(read_flag)break; // Query once for the read request.
 				}
 				for(i = 0 ; i < count ; i++){
-								pthread_join(thread_array[i], (void **) &(status[i]));
+								pthread_join(thread_array[i], NULL);
 								if(read_flag)break; // Query once for the read request
 				}
 				//    pthread_exit(0);
