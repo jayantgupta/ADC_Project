841f4ca5e8e25e9071af9203159243e57bd4aad1
Jayant Gupta <jayant27290@gmail.com>
2015-12-08 20:42:27 -0800

    Adding Sync mechanism.

--- project_4/acceptor.c
+++ project_4/acceptor.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
@@ -17,14 +18,17 @@ long unsigned int promised_id;// greatest promised id
 long unsigned int current_accepted_id;// greatest accepted id seen by acceptor
 const char *INIT_Promise = "INIT_Promise"; // store greatest promised id in this file
 const char *INIT_Accepted = "INIT_Accepted"; // store greatest accepted id in this file
+char sync_log[20000][60]; // stores the requests learnt till now.
 
+int create_tcp_connection(char *IP, char *port);
 void init(long unsigned int prom_id, long unsigned int accep_id);
 int create_tcp_server(char *port);
 char **get_tokens(char *recv_buff);
 void store_buffer(const char *buffer_file, long unsigned int ID);
 char *learner(char *);
-char *promise_message(long unsigned int promised_id);
-char *accepted_message(long unsigned int current_accepted_id);
+char *build_message(char *msg, long unsigned int promised_id);
+void server_sync(long unsigned int id);
+void serve_sync_request(int sockfd, long unsigned int current_id);
 
 int main(int argc, char *argv[]){
 				if(argc != 2){
@@ -41,7 +45,17 @@ int main(int argc, char *argv[]){
 
 				/* call init function to initialize the values */
 				init(promised_id, current_accepted_id);
+				server_sync(current_accepted_id);
 				//sync(current_accepted_id); // To start syncing.
+				/*
+					send sync request to the known servers.
+					They respond with their logs, if their ID is greater than this server's id.
+					Otherwise, they abandon the task.
+
+					When they receive the sync request does the same thing as described above but 
+					instead sends its own command history.
+
+				*/
 				sockfd = create_tcp_server(argv[1]);
 
 				/* server listening */
@@ -52,13 +66,18 @@ int main(int argc, char *argv[]){
 								/* accept the connection */
 								newsock = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
 								printf("Accepted Request\n");
-								// Serve first request.Check if sequence number less than its own stored seq number.
+								// Serve first request
+								// Check if sequence number less than its own stored seq number
 								/* Phase 1 - receive proposal */ 
 
 								recv_bytes = recv(newsock, recv_buff, 1024, 0);
 								recv_buff[recv_bytes] = '\0';
 								printf("%s\n", recv_buff);
 								char ** allTokens = get_tokens(recv_buff);             
+								if(strcmp(allTokens[0], "SYNC") == 0){
+									serve_sync_request(newsock, atoi(allTokens[1]));
+									continue;
+								}
 								printf("The receieve proposer id is : %s \n", allTokens[1]);
 							
 								long unsigned int received_prop_id = atol(allTokens[1]);
@@ -73,7 +92,7 @@ int main(int argc, char *argv[]){
 								}
 												
 								/* send promise message */
-								char *promisemessage = promise_message(promised_id);
+								char *promisemessage = build_message("promise:", promised_id);
 								printf("Promise Message is : %s\n", promisemessage);
 								send(newsock, promisemessage, strlen(promisemessage), 0);// Send back the promise.
 
@@ -91,9 +110,13 @@ int main(int argc, char *argv[]){
 												current_accepted_id = received_accept_id; 
 												/* store accepted id in INIT_Accepted file */
 												store_buffer(INIT_Accepted, current_accepted_id);
+												/* add values to sync log*/
+												strcpy(sync_log[current_accepted_id], accept_id);
+												strcat(sync_log[current_accepted_id], ":");
+												strcat(sync_log[current_accepted_id], request);
 												
 												/* send accepted message to proposer */
-												char *acceptedmessage = accepted_message(current_accepted_id);
+												char *acceptedmessage = build_message("accepted:", current_accepted_id);
 												printf("Accepted Message is : %s\n", acceptedmessage); 
 												send(newsock, acceptedmessage, strlen(acceptedmessage), 0);
 								}
@@ -112,6 +135,101 @@ int main(int argc, char *argv[]){
 				close(sockfd);
 				return 0;
 }
+
+char * build_message(char *msg, long unsigned int ID){
+	char str[15];
+	sprintf(str, "%lu", ID);
+	char *message = (char *)malloc(sizeof(char) * 50);
+	strcpy(message, msg);
+	strcat(message, str);
+	return message;
+}
+
+void serve_sync_request(int newsock, long unsigned int sync_accepted_id){
+	int recv_bytes, i;
+	char recv_buff[1024];
+	char **allTokens;
+	if(sync_accepted_id >= current_accepted_id){
+		send(newsock, "NO_DATA", strlen("NO_DATA"), 0);
+	}	
+	else{
+		send(newsock, "DATA", strlen("DATA"), 0);
+		recv_bytes = recv(newsock, recv_buff, 1024, 0);
+		allTokens = get_tokens(recv_buff);
+		if(strcmp(allTokens[0], "READY") == 0){
+			// send all the commands for sync-up.
+			for(i = 0 ; i< current_accepted_id ; i++){
+				send(newsock, sync_log[i], strlen(sync_log[i]), 0);
+			}
+			char *donemessage = build_message("DONE:", current_accepted_id);
+			send(newsock, donemessage, strlen(donemessage), 0);
+		}
+	}
+	close(newsock);
+	return;
+}
+
+void server_sync(long unsigned int ID){
+	char *ip[] = {"localhost", "localhost"};
+	char *ports[] = {"10000", "10001"};
+	int count = sizeof(ip) / sizeof(ip[0]);
+	int i, recv_bytes;
+	char recv_buff[1024];
+	for(i = 0 ; i < count ; i++){
+			int sockfd = create_tcp_connection(ip[i], ports[i]);
+			char *syncmessage = build_message("SYNC:", ID);
+			send(sockfd, syncmessage, strlen(syncmessage), 0); // syncing.
+			recv_bytes = recv(sockfd, recv_buff, 1024, 0);
+			char **allTokens = get_tokens(recv_buff);
+			if(strcmp(allTokens[0], "NO_DATA") == 0){
+				continue;
+			}
+			else if(strcmp(allTokens[0], "DATA") == 0){
+				send(sockfd, "READY", strlen("READY"), 0);
+				char *id, *request;
+				do{
+					recv_bytes = recv(sockfd, recv_buff, 1024, 0);
+					allTokens = get_tokens(recv_buff);
+					id = allTokens[0];
+					request = allTokens[1]; //extracting the request here.
+					if(strcmp(request, "DONE") != 0){
+						learner(request);
+					}
+				}while(strcmp(request, "DONE") == 0);
+			}
+	}
+}
+
+// Establishes a tcp connection with a tcp server on the given IP and port.
+int create_tcp_connection(char *IP, char *port){
+				int sockfd;
+				printf("Connect to %s %s \n", IP, port);
+				struct hostent *host = (struct hostent *)gethostbyname(IP);
+				struct sockaddr_in server_addr;
+				if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+								perror("Socket");
+								printf("Unable to create socket\n");
+								int exit_status  = 1;
+								pthread_exit(&exit_status);
+				}
+				/* creating a socket */
+				server_addr.sin_family = AF_INET;
+				server_addr.sin_port = htons(atoi(port));
+				server_addr.sin_addr = *((struct in_addr *)host->h_addr);
+				bzero(&(server_addr.sin_zero),8);
+				printf("Connection.... init\n");
+				/*connect the socket to address of server*/
+				if (connect(sockfd, (struct sockaddr *)&server_addr,sizeof(struct sockaddr)) == -1) 
+				{
+								perror("Failed to connect");
+								printf("connection failed\n");
+								int exit_status = 1;
+								pthread_exit(&exit_status);
+				}
+				printf("Connection Done !!!");
+				return sockfd;
+}
+
 //init method to initialize values on node reboot */
 void init(long unsigned int prom_id, long unsigned int accep_id){
 				/* read and initialize promise id*/
@@ -192,34 +310,14 @@ void store_buffer(const char *buffer_file, long unsigned int ID){
 				fclose(fp);
 }
 
-char *promise_message(long unsigned int promised_id){
-				char str[15]; 
-				sprintf(str, "%lu", promised_id);              
-				char *promisemessage = (char *)malloc(sizeof(char)*50);
-				strcpy(promisemessage,  "promise:");
-				strcat(promisemessage, str);// This is the promise message
-				return promisemessage;
-
-}
-
-char *accepted_message(long unsigned int current_accepted_id){
-				char accepted_id_for_msg[15];
-				sprintf(accepted_id_for_msg, "%lu", current_accepted_id); 
-				char *acceptedmessage = (char *)malloc(sizeof(char)*50);
-				strcpy(acceptedmessage,  "accepted:");
-				strcat(acceptedmessage, accepted_id_for_msg);// This is the accepted message
-				return acceptedmessage;
-}
-
-
 // Takes the first request and responds as ACK.
 // Assuming all the errors are checked at the coordinator-server
 char * learner(char * request){
 		printf("Executing Request : %s\n", request);
 		/* code to validate the request*/
-		const char s[2] = ":";
+/*		const char s[2] = ":";
 		char *token;
-		/* get the first token */
+//		get the first token 
 		token = strtok(request, s);            
 		char allTokens[3][26];             
 		int i = 0;
@@ -229,7 +327,8 @@ char * learner(char * request){
 						strcpy(allTokens[i],token);
 						token = strtok(NULL, s);
 						i++;
-		}
+		}*/
+		char ** allTokens = get_tokens(request);
 		if(strcmp(allTokens[0],"GET") == 0){ 
 						printf("Called method GET\n");
 						int key = atoi(allTokens[1]);
